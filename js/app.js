f6516328e27044bf48004f18fde58c755833baf7
MUTHAKABRIAN <muthakabrian@gmail.com>
2020-05-11 00:54:35 +0300

    add hover effect on portfolio

--- js/app.js
+++ js/app.js
@@ -34,3 +34,71 @@ $(document).ready(function(){
       $(this).hide();
     });
   });
+
+  //portfolio
+  $(document).ready(function(){
+  $(".portfolio-box-1").mouseenter(function(){
+    $(".description-1").show(200);
+  });
+  $(".portfolio-box-1").mouseleave(function() {
+    $(".description-1").hide(1000);
+  });
+});
+
+$(document).ready(function(){
+  $(".portfolio-box-2").mouseenter(function(){
+    $(".description-2").show(200);
+  });
+  $(".portfolio-box-2").mouseleave(function() {
+    $(".description-2").hide(1000);
+  });
+});
+$(document).ready(function(){
+  $(".portfolio-box-3").mouseenter(function(){
+    $(".description-3").show(200);
+  });
+  $(".portfolio-box-3").mouseleave(function() {
+    $(".description-3").hide(1000);
+  });
+});
+$(document).ready(function(){
+  $(".portfolio-box-4").mouseenter(function(){
+    $(".description-4").show(200);
+  });
+  $(".portfolio-box-4").mouseleave(function() {
+    $(".description-4").hide(1000);
+  });
+});
+$(document).ready(function(){
+  $(".portfolio-box-5").mouseenter(function(){
+    $(".description-5").show(200);
+  });
+  $(".portfolio-box-5").mouseleave(function() {
+    $(".description-5").hide(1000);
+  });
+});
+$(document).ready(function(){
+  $(".portfolio-box-6").mouseenter(function(){
+    $(".description-6").show(200);
+  });
+  $(".portfolio-box-6").mouseleave(function() {
+    $(".description-6").hide(1000);
+  });
+});
+$(document).ready(function(){
+  $(".portfolio-box-7").mouseenter(function(){
+    $(".description-7").show(200);
+  });
+  $(".portfolio-box-7").mouseleave(function() {
+    $(".description-7").hide(1000);
+  });
+});
+$(document).ready(function(){
+  $(".portfolio-box-8").mouseenter(function(){
+    $(".description-8").show(200);
+  });
+  $(".portfolio-box-8").mouseleave(function() {
+    $(".description-8").hide(1000);
+  });
+});
+form 
